f2c7ad359e215d4780da44c0f5b99b18562084f7
Yingqi Chen <chanwk18@hotmail.com>
2020-04-08 05:21:25 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,20 @@
 class Pokemon
+<<<<<<< HEAD
   attr_accessor :name,:id,:type,:db,:hp
 
   def initialize(id:, name:, type:, db:)
+=======
+  attr_accessor :name,:id,:type,:db
+
+  def initialize(id:,name:,type:,db:)
+>>>>>>> 6daeb78a119455291ab5511e4aced58e81b10b28
      @id=id
      @name=name
      @type=type
      @db=db
   end
 
+<<<<<<< HEAD
   def self.save(name,type,db)
 
    row=db.execute("INSERT INTO pokemon (name,type)  VALUES (?,?)", name,type)
@@ -26,6 +33,15 @@ class Pokemon
    db.execute("UPDATE pokemon SET hp=? WHERE id=?",hp,self.id)
 end
 
+=======
+
+  def self.save(name,type,db)
+
+    row=db.execute("INSERT INTO pokemon (name,type)  VALUES (?,?)", name,type)
+#binding.pry
+    #id =db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
+>>>>>>> 6daeb78a119455291ab5511e4aced58e81b10b28
 
 
 end
